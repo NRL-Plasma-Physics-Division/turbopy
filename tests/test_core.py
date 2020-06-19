5dce146aca286a80f88262feec28852f1829ce6a
Paul Adamson <paul.adamson@nrl.navy.mil>
2020-06-19 10:35:39 -0400

    add new grid test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 """Tests for turbopy/core.py"""
 from turbopy.core import Grid
 
+
 def test_grid():
     """Test initialization of the Grid class"""
     N_grid = 8
@@ -11,3 +12,15 @@ def test_grid():
     assert grid.grid_data == grid_conf
     assert grid.r_min == 0.0
     assert grid.r_max == 0.1
+
+
+def test_grid_2():
+    """Test initialization of the Grid class"""
+    N_grid = 10
+    grid_conf = {"N": N_grid,
+                 "r_min": 0,
+                 "r_max": 0.5}
+    grid = Grid(grid_conf)
+    assert grid.grid_data == grid_conf
+    assert grid.r_min == 0.0
+    assert grid.r_max == 0.5
