a96016c3069b837b7cdba2cb4b8e32fabbcc8444
Gareth Morgan <gtmorgan2003@gmail.com>
2020-07-24 14:36:38 -0400

    Update diagnostics.py

--- turbopy/diagnostics.py
+++ turbopy/diagnostics.py
@@ -40,7 +40,7 @@ class CSVOutputUtility:
         self.filename = filename
         self.buffer = np.zeros(diagnostic_size)
         self.buffer_index = 0
-        
+
     def append(self, data):
         """Append data to the buffer.
 
@@ -51,7 +51,7 @@ class CSVOutputUtility:
         """
         self.buffer[self.buffer_index, :] = data
         self.buffer_index += 1
-    
+
     def finalize(self):
         """Write the CSV data to file.
         """
@@ -69,7 +69,7 @@ class PointDiagnostic(Diagnostic):
         self.field = None
         self.output_function = None
         self.csv = None
-                
+
     def diagnose(self):
         self.output_function(self.get_value(self.field))
 
@@ -79,11 +79,11 @@ class PointDiagnostic(Diagnostic):
 
     def print_diagnose(self, data):
         print(data)
-        
+
     def initialize(self):
         # set up function to interpolate the field value
         self.get_value = self.owner.grid.create_interpolator(self.location)
-        
+
         # setup output method
         functions = {"stdout": self.print_diagnose,
                      "csv": self.csv_diagnose,
@@ -107,7 +107,7 @@ class PointDiagnostic(Diagnostic):
 class FieldDiagnostic(Diagnostic):
     def __init__(self, owner: Simulation, input_data: dict):
         super().__init__(owner, input_data)
-        
+
         self.component = input_data["component"]
         self.field_name = input_data["field"]
         self.output = input_data["output_type"] # "stdout"
@@ -117,14 +117,14 @@ class FieldDiagnostic(Diagnostic):
         self.last_dump = None
         self.diagnose = self.do_diagnostic
         self.diagnostic_size = None
-        
+
         self.field_was_found = False
 
     def check_step(self):
         if self.owner.clock.time >= self.last_dump + self.dump_interval:
             self.do_diagnostic()
             self.last_dump = self.owner.clock.time
-    
+
     def do_diagnostic(self):
         if len(self.field.shape) > 1:
             self.output_function(self.field[:, self.component])
@@ -135,10 +135,10 @@ class FieldDiagnostic(Diagnostic):
         if self.field_name in resource:
             self.field_was_found = True
             self.field = resource[self.field_name]
-    
+
     def print_diagnose(self, data):
         print(self.field_name, data)
-        
+
     def initialize(self):
         if not self.field_was_found:
             raise(RuntimeError(f"Diagnostic field {self.field_name} was not found"))
@@ -150,7 +150,7 @@ class FieldDiagnostic(Diagnostic):
             self.last_dump = 0
             self.diagnostic_size = (int(np.ceil(self.owner.clock.end_time/self.dump_interval)+1),
                                     self.owner.grid.num_points)       
-    
+
         # setup output method
         functions = {"stdout": self.print_diagnose,
                      "csv": self.csv_diagnose,
@@ -158,10 +158,10 @@ class FieldDiagnostic(Diagnostic):
         self.output_function = functions[self.input_data["output_type"]]
         if self.input_data["output_type"] == "csv":
             self.csv = CSVOutputUtility(self.input_data["filename"], self.diagnostic_size)
-    
+
     def csv_diagnose(self, data):
         self.csv.append(data)
-    
+
     def finalize(self):
         self.do_diagnostic()
         if self.input_data["output_type"] == "csv":
@@ -191,7 +191,7 @@ class GridDiagnostic(Diagnostic):
     def __init__(self, owner: Simulation, input_data: dict):
         super().__init__(owner, input_data)
         self.filename = input_data["filename"]
-            
+
     def diagnose(self):
         pass
 
