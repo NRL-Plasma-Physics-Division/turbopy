4c3219b3b26918703ae1110d56d129e88da6969a
gracetangg <gracehtang@yahoo.com>
2020-07-28 10:09:45 -0400

    added .toml file

--- tests/fixtures/block_on_spring/block_on_spring.py
+++ tests/fixtures/block_on_spring/block_on_spring.py
@@ -7,6 +7,7 @@ import pytest
 
 from turbopy import Simulation, PhysicsModule, Diagnostic
 from turbopy import CSVOutputUtility, ComputeTool
+from turbopy import construct_simulation_from_toml
 
 
 class BlockOnSpring(PhysicsModule):
@@ -129,35 +130,6 @@ def bos_run():
     ComputeTool.register("BackwardEuler", BackwardEuler)
     ComputeTool.register("LeapFrog", LeapFrog)
     
-    problem_config = {
-        # Note: grid isn't used, but "gridless" sims aren't an option yet
-        "Grid": {"N": 2, "x_min": 0, "x_max": 1},
-        "Clock": {"start_time": 0,
-                  "end_time": 10,
-                  "num_steps": 100},
-        "PhysicsModules": {
-            "BlockOnSpring": {
-                "mass": 1,
-                "spring_constant": 1,
-                "pusher": "LeapFrog",
-                "x0": [0, 1, 0],
-                "p0": [0, 0, 0],
-            }
-        },
-        "Tools": {
-            "BackwardEuler": {},
-            "LeapFrog": {}
-        },
-        "Diagnostics": {
-            # default values come first
-            "directory": "tmp/block_on_spring/output/",
-            "output_type": "csv",
-            "clock": {"filename": "time.csv"},
-            "BlockDiagnostic": [
-                {'component': 'momentum', 'filename': 'block_p.csv'},
-                {'component': 'position', 'filename': 'block_x.csv'}
-            ]
-        }
-    }
-    sim = Simulation(problem_config)
+    input_file = "tests/fixtures/block_on_spring/block_on_spring.toml"
+    sim = construct_simulation_from_toml(input_file)
     sim.run()
