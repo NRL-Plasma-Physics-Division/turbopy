fad2093de70953219b4b127fa5822bfb1232e246
gracetangg <gracehtang@yahoo.com>
2020-07-28 09:53:39 -0400

    update test_bos

--- tests/test_bos.py
+++ tests/test_bos.py
@@ -7,9 +7,9 @@ import numpy as np
 
 
 def test_bos(bos_run):
-    for filename in ['e_0.5', 'grid', 'particle_p', 'particle_x', 'time']:
-        ref_data = np.genfromtxt(f'tests/fixtures/particle_in_field/output/{filename}.csv',
+    for filename in ['block_p', 'block_x', 'time']:
+        ref_data = np.genfromtxt(f'tests/fixtures/block_on_spring/output/{filename}.csv',
                                  delimiter=',')
-        tmp_data = np.genfromtxt(f'tmp/particle_in_field/output/{filename}.csv',
+        tmp_data = np.genfromtxt(f'tmp/block_on_spring/output/{filename}.csv',
                                  delimiter=',')
         assert (np.allclose(ref_data, tmp_data, rtol=1e-05, atol=1e-08))
