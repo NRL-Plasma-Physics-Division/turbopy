cc7d449f4a6c41053bc336e026b1f2160070208a
gracetangg <gracehtang@yahoo.com>
2020-08-05 13:05:54 -0400

    fixes sone tests

--- tests/test_computetools.py
+++ tests/test_computetools.py
@@ -15,12 +15,12 @@ def test_interpolate1D(interpolator):
     x = np.arange(0, 10, 1)
     y = np.exp(x)
     xnew = np.arange(0, 1, 0.1)
-
+    
     f1 = interpolator.interpolate1D(x, y)
     f2 = interpolate.interp1d(x, y)
     assert np.allclose(f1(x), y)
     assert np.allclose(f1(xnew), f2(xnew))
-
+    
     y = np.asarray([n ** 2 for n in x])
     f1 = interpolator.interpolate1D(x, y, 'quadratic')
     f2 = interpolate.interp1d(x, y, 'quadratic')
@@ -29,7 +29,7 @@ def test_interpolate1D(interpolator):
 
 
 @pytest.fixture
-def centered_finite():
+def fin_diff():
     dic = {"Grid": {"N": 10, "r_min": 0, "r_max": 10},
            "Clock": {"start_time": 0,
                      "end_time": 10,
@@ -42,65 +42,59 @@ def centered_finite():
     return FiniteDifference(sim, {'type': 'FiniteDifference', 'method': 'centered'})
 
 
-@pytest.fixture
-def upwind_finite():
-    dic = {"Grid": {"N": 10, "r_min": 1, "r_max": 11},
-           "Clock": {"start_time": 0,
-                     "end_time": 10,
-                     "num_steps": 100},
-           "Tools": {},
-           "PhysicsModules": {},
-           }
-    sim = Simulation(dic)
-    sim.run()
-    return FiniteDifference(sim, {'type': 'FiniteDifference', 'method': 'upwind_left'})
-
-
-def test_setup_ddx_returns_respected_functions(centered_finite, upwind_finite):
-    center = centered_finite.setup_ddx()
-    upwind = upwind_finite.setup_ddx()
-    assert center == centered_finite.centered_difference
-    assert upwind == upwind_finite.upwind_left
-    
+def test_setup_ddx_returns_respected_functions(fin_diff):
     y = np.arange(0, 10)
+    center = fin_diff.setup_ddx()
+    
+    assert center == fin_diff.centered_difference
     assert center(y).shape == (10,)
+    assert np.allclose(center(y), fin_diff.centered_difference(y))
+    
+    fin_diff.input_data['method'] = 'upwind_left'
+    upwind = fin_diff.setup_ddx()
+    assert upwind == fin_diff.upwind_left
     assert upwind(y).shape == (10,)
-    assert np.allclose(center(y), centered_finite.centered_difference(y))
-    assert np.allclose(upwind(y), upwind_finite.upwind_left(y))
+    assert np.allclose(upwind(y), fin_diff.upwind_left(y))
 
 
-def test_ddx(centered_finite):
-    N = centered_finite.owner.grid.num_points
-    g = 1 / (2.0 * centered_finite.dr)
-    d = centered_finite.ddx()
+def test_ddx(fin_diff):
+    N = fin_diff.owner.grid.num_points
+    g = 1 / (2.0 * fin_diff.dr)
+    d = fin_diff.ddx()
     assert d.shape == (N, N)
     assert np.allclose(d.toarray(), sparse.dia_matrix(([np.zeros(N) - g, np.zeros(N) + g], [-1, 1]),
                                                       shape=(N, N)).toarray())
 
 
-def test_radial_curl(upwind_finite):
-    N = upwind_finite.owner.grid.num_points
-    d = upwind_finite.radial_curl()
-    d_array = d.toarray()
+def test_radial_curl(fin_diff):
+    N = fin_diff.owner.grid.num_points
+    dr = fin_diff.owner.grid.dr
+    r = fin_diff.owner.grid.r
+    g = 1 / (2.0 * dr)
+    
+    below = np.append(-g * (r[:-1] / r[1:])[:-1], [0.0, 0.0])
+    diag = np.append(np.zeros(N - 1), [1/dr])
+    above = np.append([0.0, 2.0 / dr], g * (r[1:] / r[:-1])[1:])
+    
+    d = fin_diff.radial_curl()
     assert d.shape == (N, N)
-    for ind in range(N - 1):
-        assert d_array[ind + 1][ind] == d.data[0][ind]
-    for ind in range(N):
-        assert d_array[ind][ind] == d.data[1][ind]
-    for ind in range(N - 1):
-        assert d_array[ind][ind + 1] == d.data[2][ind + 1]
+    for actual, expected in zip(d.data, [below, diag, above]):
+        assert np.allclose(actual, expected)
 
-def test_del2_radial(upwind_finite):
-    N = upwind_finite.owner.grid.num_points
-    dr = upwind_finite.owner.grid.dr
-    r = upwind_finite.owner.grid.r
+
+def test_del2_radial(fin_diff):
+    N = fin_diff.owner.grid.num_points
+    dr = fin_diff.owner.grid.dr
+    r = fin_diff.owner.grid.r
     g1 = 1 / (2.0 * dr)
     g2 = 1 / (dr ** 2)
+    
     below = np.append(-g1 / r[1:], [-g1]) + (g2 * np.ones(N))
-    above = np.append([g1, 0], g1 / r[1:-1]) + np.append([g2, g2 * 2], g2 * np.ones(N - 2))
+    above = (np.append([g1, 0], g1 / r[1:-1]) +
+             np.append([g2, g2 * 2], g2 * np.ones(N - 2)))
     diag = -2 * g2 * np.ones(N)
     
-    d = upwind_finite.del2_radial()
+    d = fin_diff.del2_radial()
     d_array = d.toarray()
     assert d.shape == (N, N)
     for ind in range(N - 1):
@@ -111,88 +105,99 @@ def test_del2_radial(upwind_finite):
         assert d_array[ind][ind + 1] == above[ind + 1]
 
 
-def test_del2(upwind_finite):
-    N = upwind_finite.owner.grid.num_points
-    d = upwind_finite.del2()
-    d_array = d.toarray()
+def test_del2(fin_diff):
+    N = fin_diff.owner.grid.num_points
+    dr = fin_diff.owner.grid.dr
+    g = 1 / (dr ** 2)
+    
+    below = g * np.ones(N)
+    diag = -2 * g * np.ones(N)
+    above = g * np.ones(N)
+    above[1] *= 2
+    
+    d = fin_diff.del2()
     assert d.shape == (N, N)
-    for ind in range(N - 1):
-        assert d_array[ind + 1][ind] == d.data[0][ind]
-    for ind in range(N):
-        assert d_array[ind][ind] == d.data[1][ind]
-    for ind in range(N - 1):
-        assert d_array[ind][ind + 1] == d.data[2][ind + 1]
+    for actual, expected in zip(d.data, [below, diag, above]):
+        assert np.allclose(actual, expected)
 
 
-def test_ddr(centered_finite):
-    N = centered_finite.owner.grid.num_points
-    d = centered_finite.ddr()
-    d_array = d.toarray()
+def test_ddr(fin_diff):
+    N = fin_diff.owner.grid.num_points
+    dr = fin_diff.owner.grid.dr
+    g1 = 1 / (2.0 * dr)
+    
+    below = -g1 * np.ones(N)
+    above = g1 * np.ones(N)
+    above[1] = 0
+    
+    d = fin_diff.ddr()
     assert d.shape == (N, N)
-    for ind in range(N - 1):
-        assert d_array[ind + 1][ind] == d.data[0][ind]
-    for ind in range(N - 1):
-        assert d_array[ind][ind + 1] == d.data[1][ind + 1]
+    for actual, expected in zip(d.data, [below, above]):
+        assert np.allclose(actual, expected)
 
 
-def test_BC_left_extrap(centered_finite):
-    N = centered_finite.owner.grid.num_points
-    d = centered_finite.BC_left_extrap()
-    d_array = d.toarray()
+def test_BC_left_extrap(fin_diff):
+    N = fin_diff.owner.grid.num_points
+    
+    diag = np.append(0, np.ones(N-1))
+    above = np.append([0.0, 2.0], np.zeros(N-2))
+    above2 = np.append([0.0, 0.0, -1.0], np.zeros(N-3))
+    
+    d = fin_diff.BC_left_extrap()
     assert d.shape == (N, N)
-    for ind in range(N):
-        assert d_array[ind][ind] == d.data[0][ind]
-    for ind in range(N - 1):
-        assert d_array[ind][ind + 1] == d.data[1][ind + 1]
-    for ind in range(N - 2):
-        assert d_array[ind][ind + 2] == d.data[2][ind + 2]
+    for actual, expected in zip(d.data, [diag, above, above2]):
+        assert np.allclose(actual, expected)
 
 
-def test_BC_left_avg(centered_finite):
-    N = centered_finite.owner.grid.num_points
-    d = centered_finite.BC_left_avg()
-    d_array = d.toarray()
+def test_BC_left_avg(fin_diff):
+    N = fin_diff.owner.grid.num_points
+
+    diag = np.append(0, np.ones(N - 1))
+    above = np.append([0.0, 1.5], np.zeros(N - 2))
+    above2 = np.append([0.0, 0.0, -0.5], np.zeros(N - 3))
+    
+    d = fin_diff.BC_left_avg()
     assert d.shape == (N, N)
-    for ind in range(N):
-        assert d_array[ind][ind] == d.data[0][ind]
-    for ind in range(N - 1):
-        assert d_array[ind][ind + 1] == d.data[1][ind + 1]
-    for ind in range(N - 2):
-        assert d_array[ind][ind + 2] == d.data[2][ind + 2]
+    for actual, expected in zip(d.data, [diag, above, above2]):
+        assert np.allclose(actual, expected)
 
 
-def test_BC_left_quad(centered_finite):
-    N = centered_finite.owner.grid.num_points
-    d = centered_finite.BC_left_quad()
-    d_array = d.toarray()
+def test_BC_left_quad(fin_diff):
+    N = fin_diff.owner.grid.num_points
+    r = fin_diff.owner.grid.r
+    R = (r[1]**2 + r[2]**2)/(r[2]**2 - r[1]**2)/2
+    
+    diag = np.append(0, np.ones(N - 1))
+    above = np.append([0.0, 0.5 + R], np.zeros(N - 2))
+    above2 = np.append([0.0, 0.0, 0.5 - R], np.zeros(N - 3))
+    
+    d = fin_diff.BC_left_quad()
     assert d.shape == (N, N)
-    for ind in range(N):
-        assert d_array[ind][ind] == d.data[0][ind]
-    for ind in range(N - 1):
-        assert d_array[ind][ind + 1] == d.data[1][ind + 1]
-    for ind in range(N - 2):
-        assert d_array[ind][ind + 2] == d.data[2][ind + 2]
+    for actual, expected in zip(d.data, [diag, above, above2]):
+        assert np.allclose(actual, expected)
 
 
-def test_BC_left_flat(centered_finite):
-    N = centered_finite.owner.grid.num_points
-    d = centered_finite.BC_left_flat()
-    d_array = d.toarray()
+def test_BC_left_flat(fin_diff):
+    N = fin_diff.owner.grid.num_points
+   
+    diag = np.append(0, np.ones(N - 1))
+    above = np.append([0.0, 1], np.zeros(N - 2))
+    
+    d = fin_diff.BC_left_flat()
     assert d.shape == (N, N)
-    for ind in range(N):
-        assert d_array[ind][ind] == d.data[0][ind]
-    for ind in range(N - 1):
-        assert d_array[ind][ind + 1] == d.data[1][ind + 1]
+    for actual, expected in zip(d.data, [diag, above]):
+        assert np.allclose(actual, expected)
 
 
-def test_BC_right_extrap(centered_finite):
-    N = centered_finite.owner.grid.num_points
-    d = centered_finite.BC_right_extrap()
-    d_array = d.toarray()
+def test_BC_right_extrap(fin_diff):
+    N = fin_diff.owner.grid.num_points
+    
+    below2 = np.append(np.zeros(N - 3), [-1.0, 0.0, 0.0])
+    below = np.append(np.zeros(N - 2), [2.0, 0.0])
+    diag = np.append(np.ones(N - 1), 0)
+    
+    d = fin_diff.BC_right_extrap()
     assert d.shape == (N, N)
-    for ind in range(N - 2):
-        assert d_array[ind + 2][ind] == d.data[0][ind]
-    for ind in range(N - 1):
-        assert d_array[ind + 1][ind] == d.data[1][ind]
-    for ind in range(N):
-        assert d_array[ind][ind] == d.data[2][ind]
+    for actual, expected in zip(d.data, [below2, below, diag]):
+        assert np.allclose(actual, expected)
+
